1bd988645c4f1f60f312f461b743cc38690c1e1d
Sherly789 <sherlysh789>
2016-07-29 18:43:46 -0700

    add cshmtl to enter stylist name

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,7 +8,34 @@ namespace HairSalon
   {
     public Homemodule()
     {
-      Get["/hi"] = _ => "hello";
+      Get["/"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["index.cshtml", AllStylists];
+      };
+
+      Get["/stylists"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["stylists.cshtml", AllStylists];
+      };
+
+      Get["/stylists/new"] = _ => {
+        return View["stylists_form.cshtml"];
+      };
+
+      // Post["/stylists/new"] = _ => {
+      //   Stylists newStylists = new Stylists(Request.Form["stylist-name"]);
+      //   newStylists.Save();
+      //   return View["success.cshtml"];
+      // };
+
+      // Get["/stylists/{id}"] = parameters => {
+      //   Dictionary<string, object> model = new Dictionary<string, object>();
+      //   var SelectedStylist = Stylist.Find(parameters.id);
+      //   var StylistNames = SelectedStylist.GetNames();
+      //   model.Add("stylist", SelectedStylist);
+      //   model.Add("names", StylistNames);
+      //   return View["stylist.cshtml", model];
+      // };
     }
   }
 }
